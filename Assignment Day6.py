e766367deaa9ca535987491aaa4ff42982157a1b
samshelke13 <68281876+samshelke13@users.noreply.github.com>
2020-07-28 11:11:11 +0530

    Assignment Day6

---
+++ Assignment Day6.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+list1=[1,2,3,4,5,7,8]
+list2=["a","b","c","d",]
+d={}
+for i in range(len(list2)):
+     list1.append(list2[i])
+for i in list1:
+    d[i]=i
+print(d)
+
+
+# In[ ]:
+
+
+
+
